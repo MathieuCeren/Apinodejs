7346e057b4da8fe70d8fa833e98d7ea8dffd7123
MathieuCeren <cerenmathieu@gmail.com>
2020-05-20 12:32:56 +0200

    remove key

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 module.exports = require('knex')({
     client: 'pg',
     connection: {
-        host: 'equipe-02.cqf0cfj75jd4.eu-west-3.rds.amazonaws.com',
-        user: 'student',
-        password: '4vqzw94DCpJKZBem22YD75KgGD',
+        host: '',
+        user: '',
+        password: '',
         database: 'postgres'
     }
 });
